c10e5fcbe9c93097b419f74734b8144afa6d7309
gn0d3z04m <styrelwho@gmail.com>
2025-05-24 14:59:52 +0300

    Update test_account.cpp

--- tests/test_account.cpp
+++ tests/test_account.cpp
@@ -2,24 +2,36 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
-class MockAccount : public Account {
+class AccountMock : public Account {
 public:
-    MockAccount(int id, int balance) : Account(id, balance) {}
-    MOCK_CONST_METHOD0(GetBalance, int());
-    MOCK_METHOD1(ChangeBalance, void(int));
-    MOCK_METHOD0(Lock, void());
-    MOCK_METHOD0(Unlock, void());
+	AccountMock(int id, int balance) : Account(id, balance) {}
+	MOCK_CONST_METHOD0(GetBalance, int());
+	MOCK_METHOD1(ChangeBalance, void(int diff));
+	MOCK_METHOD0(Lock, void());
+	MOCK_METHOD0(Unlock, void());
 };
 
-TEST(AccountTest, LockUnlock) {
-    Account acc(1, 100);
-    acc.Lock();
-    ASSERT_NO_THROW(acc.ChangeBalance(10));
-    acc.Unlock();
-    ASSERT_THROW(acc.ChangeBalance(10), std::runtime_error);
+TEST(Account, Mock) {
+	AccountMock acc(1, 100);
+	EXPECT_CALL(acc, GetBalance()).Times(1);
+	EXPECT_CALL(acc, ChangeBalance(testing::_)).Times(2);
+	EXPECT_CALL(acc, Lock()).Times(2);
+	EXPECT_CALL(acc, Unlock()).Times(1);
+	acc.GetBalance();
+	acc.ChangeBalance(100);
+	acc.Lock();
+	acc.ChangeBalance(100);
+	acc.Lock();
+	acc.Unlock();
 }
 
-TEST(AccountTest, GetBalance) {
-    Account acc(1, 200);
-    ASSERT_EQ(acc.GetBalance(), 200);
+TEST(Account, SimpleTest) {
+	Account acc(1, 100);
+	EXPECT_EQ(acc.id(), 1);
+	EXPECT_EQ(acc.GetBalance(), 100);
+	EXPECT_THROW(acc.ChangeBalance(100), std::runtime_error);
+	EXPECT_NO_THROW(acc.Lock());
+	acc.ChangeBalance(100);
+	EXPECT_EQ(acc.GetBalance(), 200);
+	EXPECT_THROW(acc.Lock(), std::runtime_error);
 }
