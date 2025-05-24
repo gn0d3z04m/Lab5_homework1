4fca55fc1025ee2d0b4e1e358d9ed0d5c74d9cbd
gn0d3z04m <styrelwho@gmail.com>
2025-05-24 15:01:12 +0300

    Update test_transaction.cpp

--- tests/test_transaction.cpp
+++ tests/test_transaction.cpp
@@ -2,28 +2,36 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
-class MockTransaction : public Transaction {
+class TransactionMock : public Transaction {
 public:
-    MOCK_METHOD3(SaveToDataBase, void(Account&, Account&, int));
+	MOCK_METHOD3(Make, bool(Account& from, Account& to, int sum));
 };
 
-TEST(TransactionTest, MakeTransfer) {
-    Account from(1, 500);
-    Account to(2, 300);
-    Transaction ts;
-    
-    from.Lock();
-    to.Lock();
-    
-    ASSERT_TRUE(ts.Make(from, to, 150));
-    ASSERT_EQ(from.GetBalance(), 500 - 150 - ts.fee());
-    ASSERT_EQ(to.GetBalance(), 300 + 150);
+TEST(Transaction, Mock) {
+	TransactionMock tr;
+	Account ac1(1, 50);
+	Account ac2(2, 500);
+	EXPECT_CALL(tr, Make(testing::_, testing::_, testing::_))
+	.Times(6);
+	tr.set_fee(100);
+	tr.Make(ac1, ac2, 199);
+	tr.Make(ac2, ac1, 500);
+	tr.Make(ac2, ac1, 300);
+	tr.Make(ac1, ac1, 0);
+	tr.Make(ac1, ac2, -4);
+	tr.Make(ac1, ac2, 99);
 }
 
-TEST(TransactionTest, InvalidTransfer) {
-    Account acc1(1, 100);
-    Account acc2(1, 200);
-    Transaction ts;
-    
-    ASSERT_THROW(ts.Make(acc1, acc2, 50), std::logic_error);
+TEST(Transaction, SimpleTest) {
+	Transaction tr;
+	Account ac1(1, 50);
+	Account ac2(2, 500);
+	tr.set_fee(100);
+	EXPECT_EQ(tr.fee(), 100);
+	EXPECT_THROW(tr.Make(ac1, ac1, 0), std::logic_error);
+	EXPECT_THROW(tr.Make(ac1, ac2, -1), std::invalid_argument);
+	EXPECT_THROW(tr.Make(ac1, ac2, 99), std::logic_error);
+	EXPECT_FALSE(tr.Make(ac1, ac2, 199));
+	EXPECT_FALSE(tr.Make(ac2, ac1, 500));
+	EXPECT_TRUE(tr.Make(ac2, ac1, 300));
 }
